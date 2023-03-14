4004ca5446a251e03d2f5fe8a23714d088eebedd
HarHamz <harryhh246@gmail.com>
2023-03-14 23:49:11 +0700

    task 2 done

--- task2.cpp
+++ task2.cpp
@@ -1,10 +1,22 @@
+/*
+Suppose you entered a 6 digit number. Write a program to display a number whose digits are 2 greater than the corresponding digits of the entered number.
+For example, if you entered number is 569678, then the displayed number should be 781890.
+*/
 #include <iostream>
 
 using namespace std;
 
 int main(){
+    system("cls");
 
-    
+    int input;
+
+    cout << "insert a 6-digit number: ";
+    cin >> input;    
+
+    input=input+2;
+
+    cout << "your number added by 2 is " << input;
 
     return 0;
 }
--- task3.cpp
+++ task3.cpp
@@ -1,3 +1,7 @@
+/*
+Write a program to calculate the sum of the digits of a 4-digit number which is entered from keyboard. E.g.-
+INPUT : 1234        OUTPUT : 10
+*/
 #include <iostream>
 
 using namespace std;
