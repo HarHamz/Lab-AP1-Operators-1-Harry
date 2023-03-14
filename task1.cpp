5a995e98d541d0af9d1c0e3520e1ba7dbb32dcdf
HarHamz <harryhh246@gmail.com>
2023-03-14 23:36:56 +0700

    task 1 done

--- task1.cpp
+++ task1.cpp
@@ -1,10 +1,29 @@
+/*
+What would be result:
+Write a program to enter a 4 digit number which is 1233 from keyboard. 
+Add 8 to the number and then divide it by 3. 
+Now, the modulus of that number is taken with 5 
+and then multiply the resultant value by 5. 
+Display the final result.
+*/
 #include <iostream>
 
 using namespace std;
 
 int main(){
+    system("cls");
 
-    
+    int input;
+
+    cout << "insert a 4-digit number: ";
+    cin >> input;
+
+    input=input+8;
+    input=input/3;
+    input=input%5;
+    input=input*5;
+
+    cout << "The number now is " << input;
 
     return 0;
 }
