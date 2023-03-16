00372134439058b3e66f45e35b31dbfba42926f0
HarHamz <harryhh246@gmail.com>
2023-03-16 22:43:44 +0700

    task 4 letsgo

---
+++ task4.cpp
@@ -0,0 +1,18 @@
+/*
+Write a function that takes in a time in seconds (as an integer) and returns a string in the format "HH:MM:SS". For example, if the input is 3661, the output should be "01:01:01".
+
+Here are some additional requirements and constraints:
+1. The input time will be a non-negative integer.
+2. The output should always have two digits for hours, minutes, and seconds (for example, "01" instead of "1").
+*/
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    system("cls");
+    
+    
+    
+    return 0;
+}
